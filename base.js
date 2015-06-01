b26808a9c63320bec48b116b7b6176ff50313e26
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2015-06-01 17:40:33 -0400

    animated scrolling to sections of main page

--- base.js
+++ base.js
@@ -1,9 +1,5 @@
-// $('a#work').click(function() {
-//      console.log("hats");
-//      $('html,body').animate({scrollTop: $("div#work").offset().top}, 500);
-// }); 
-
-// $("div#project-carla").hover(function() {
-//   console.log("carla hover");
-//   $(this).text = "Hi"
-// })
+$('#work-nav').click(function () {
+  console.log("sdfsdfs")
+    $('body,html').animate({ scrollTop: $('body').height() }, 500);
+    return false;
+});
