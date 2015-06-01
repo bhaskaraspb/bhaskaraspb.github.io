ba666274035fdf9c1b2a61b2eff5d336d35b516d
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2015-06-01 13:36:11 -0400

    front-end overhaul

--- base.js
+++ base.js
@@ -2,3 +2,8 @@
 //      console.log("hats");
 //      $('html,body').animate({scrollTop: $("div#work").offset().top}, 500);
 // }); 
+
+// $("div#project-carla").hover(function() {
+//   console.log("carla hover");
+//   $(this).text = "Hi"
+// })
